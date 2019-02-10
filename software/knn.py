c26338ae9a9b3167a3c7b3ea702920b45ead68b5
XieJihui <xjh125470939@gmail.com>
2019-02-10 21:52:27 +0800

    update knn algo

--- software/knn.py
+++ software/knn.py
@@ -20,26 +20,42 @@ def handleDataset(filename, split, trainingSet=[], testSet=[]):
             else:
                 testSet.append(dataset[i])
     
-k = 10
+def kNN_algo(trainingSet, testPoint):
+    prediction = {}
+    distance = {}
+    # compute euclidean distance between the ith data point and each training point
+    for j in range(len(trainingSet)):
+        distance[j] = euclideanDistance(testPoint, trainingSet[j])
+    # sort the training data according to the distance 
+    distance = sorted(distance.items(), key=operator.itemgetter(1))
+    # find the class of the k nearest neighbors
+    # x is the xth point
+    # distance[x][0] is the index of the point in the list of trainingSet
+    # assume the last value is the class
+    for x in range(k):
+        classOfPoint = int(trainingSet[distance[x][0]][len(trainingSet[distance[x][0]])-1])
+        if classOfPoint in prediction:  
+            prediction[classOfPoint] += 1
+        else:
+            prediction[classOfPoint] = 1
+    # sort the prediction dictionary according the appearance 
+    prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
+    # return the class which appeares the most times in the k nearest neighbors
+    return prediction[0][0]
+    
+
+
+k = 5
+filePath = 'C:\\Users\\xiejihui\\Desktop\\3002\\test_data\\heart.csv'
+splitRatio = 0.75  #proportion of training sets
 trainingSet = []
 testSet = []
+
+handleDataset(filePath, splitRatio, trainingSet, testSet)
+
 correct_prediction = 0
-handleDataset('C:\\Users\\xiejihui\\Desktop\\3002\\test_data\\heart.csv', 0.75, trainingSet, testSet)
 for i in range(len(testSet)):
-    distance = {}
-    class_one = 0
-    prediction = 0
-    for j in range(len(trainingSet)):
-        distance[j] = euclideanDistance(testSet[i], trainingSet[j])
-    distance= sorted(distance.items(), key=operator.itemgetter(1))
-    
-    for x in range(k):
-        n = trainingSet[distance[x][0]][len(trainingSet[distance[x][0]])-1]
-        if int(n) == 1:  
-            class_one += 1
- 
-    if class_one > k/2:
-        prediction = 1
+    prediction = kNN_algo(trainingSet, testSet[i])
     if prediction == int(testSet[i][len(testSet[i])-1]):
         correct_prediction += 1
 
