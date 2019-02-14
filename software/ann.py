8341f9d173cfaa50c93ddf0da0fff40ab3626abf
Xie Jihui <xjh125470939@gmail.com>
2019-02-14 14:17:37 +0800

    no message

--- software/ann.py
+++ software/ann.py
@@ -6,31 +6,28 @@ import torch.nn.functional as F
 from preprocess_data import split_feature_class_as_array
 
 
-def run_ann(trainingSet, testSet):
-    trainingFeatures = []
-    trainingClass = []
-    testFeatures = []
-    testClass = []
-    split_feature_class_as_array(trainingSet, trainingFeatures, trainingClass)
-    split_feature_class_as_array(testSet, testFeatures, testClass)
+def run_ann(training_set, test_set):
+    training_feature = []
+    training_class = []
+    test_feature = []
+    test_class = []
+    split_feature_class_as_array(training_set, training_feature, training_class)
+    split_feature_class_as_array(test_set, test_feature, test_class)
 
-    n_in = len(trainingFeatures[0])
+    n_in = len(training_feature[0])
     n_h = 5
     n_out = 1
-
-    x = torch.FloatTensor(trainingFeatures)
-    y = torch.FloatTensor(trainingClass)
 
     model = nn.Sequential(nn.Linear(n_in, n_h),
                         nn.ReLU(),
                         nn.Linear(n_h, n_out),
                         nn.Sigmoid())
-    print(model)
-
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-    for epoch in range(10000):
+    x = torch.FloatTensor(training_feature)
+    y = torch.FloatTensor(training_class)
+    for epoch in range(2000):
         # Forward Propagation
         y_pred = model(x)
         # Compute and print loss
@@ -38,25 +35,35 @@ def run_ann(trainingSet, testSet):
         # print('epoch: ', epoch,' loss: ', loss.item())
         # Zero the gradients
         optimizer.zero_grad()
-        
         # perform a backward pass (backpropagation)
         loss.backward()
-        
         # Update the parameters
         optimizer.step()
     
-    test = torch.FloatTensor(testFeatures)
-    pred = model(test)
+    x = torch.FloatTensor(test_feature)
+    y = torch.FloatTensor(test_class)
+    for epoch in range(500):
+        # Forward Propagation.
+        y_pred = model(x)
+        # Compute and print loss.
+        loss = criterion(y_pred, y)
+        # print ('epoch: ', epoch, ' loss: ', loss.item())
+        # Zero the gradients.
+        optimizer.zero_grad()
+        # perform a backward pass (backpropagation)
+        loss.backward()
+        # Update the parameters
+        optimizer.step()
+
     correct = 0
     pred_list = []
-    for i in range(len(pred)):
-        if pred.data[0] < 0.5:
+    for i in range(len(y_pred)):
+        if y_pred.data[i] < 0.5:
              pred_list.append(0)
         else:
             pred_list.append(1)
 
     for i in range(len(pred_list)):
-        if pred_list[i] == testClass[i][0]:
+        if pred_list[i] == test_class[i][0]:
             correct += 1
-
     return correct/len(pred_list)
--- software/classifier.py
+++ software/classifier.py
@@ -3,13 +3,13 @@ from svm import run_svm
 from ann import run_ann
 from preprocess_data import process_dataset, split_feature_class_as_array
 
-data_file_path = "test_data/bezdekIris.csv"
+data_file_path = "test_data/pulsar_stars.csv"
 split_ratio = 0.75  #proportion of training sets
 training_set = []
 test_set = []
 process_dataset(data_file_path, split_ratio, training_set, test_set)
 
-accuracy = run_knn(training_set, test_set, k=5)
+# accuracy = run_knn(training_set, test_set, k=5)
 # accuracy = run_svm(training_set, test_set)
-# accuracy = run_ann(training_set, test_set)
+accuracy = run_ann(training_set, test_set)
 print(accuracy)
