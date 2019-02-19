5ce3e684ea326c36e4ccddcd67496704d391ef6f
Xie Jihui <xjh125470939@gmail.com>
2019-02-19 12:55:53 +0800

    no message

--- software/classifier.py
+++ software/classifier.py
@@ -26,7 +26,7 @@ def run_svm(X_train, X_test, y_train, y_test):
     clf.fit(X_train, y_train) 
     return clf.predict(X_test)
 
-file_name = 'bezdekIris'
+file_name = 'pulsar_stars'
 file_path = 'test_data/' + file_name + '.csv'
 dataframe = pandas.read_csv(file_path, header=None)
 dataset = dataframe.values
@@ -34,10 +34,25 @@ X = dataset[:,0:len(dataset[0])-1].astype(float)
 y = dataset[:,len(dataset[0])-1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# 93.3% for bezdekIris
+# about 65% for heart
+# about 97% for pulsar_stars
 # prediction = run_knn(X_train, X_test, y_train, y_test, k=5)
+
+# about 93.3% for bezdekIris
+# 50% - 60% for heart
+# about 97% for pulsar_stars, obviously slower than knn
 # prediction = run_svm(X_train, X_test, y_train, y_test)
-prediction = run_ann(X_train, X_test, y_train, y_test, file_name+'_model')
-# prediction = run_perceptron(X_train, X_test, y_train, y_test)
+
+# over 96.6% for bezdekIris
+# does not work for heart
+# over 98% for pulsar_stars, training is slow with large dataset, can use saved trained model
+# prediction = run_ann(X_train, X_test, y_train, y_test, file_name+'_model')
+
+# 50% - 80% for bezdekIris
+# does not work for heart
+# about 97% for pulsar_stars
+prediction = run_perceptron(X_train, X_test, y_train, y_test)
 
 matrix = confusion_matrix(y_test, prediction)
 print(matrix)
