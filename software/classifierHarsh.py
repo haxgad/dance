4190035ce3713ce532f1e30f547cd17f09a5c63b
Harsh Gadodia <harsh@u.nus.edu>
2019-03-04 16:28:05 +0800

    add information about row headers into classifier file

--- software/classifierHarsh.py
+++ software/classifierHarsh.py
@@ -111,33 +111,9 @@ run_algo(decision_tree, 'Breast_cancer_data')
 
 #breast cancer headers
 
-# radius_meanmean of distances from center to points on the perimeter
-# texture_meanstandard deviation of gray-scale values
-# perimeter_meanmean size of the core tumor
-# area_mean
-# smoothness_meanmean of local variation in radius lengths
-# compactness_meanmean of perimeter^2 / area - 1.0
-# concavity_meanmean of severity of concave portions of the contour
-# concave points_meanmean for number of concave portions of the contour
-# symmetry_mean
-# fractal_dimension_meanmean for "coastline approximation" - 1
-# radius_sestandard error for the mean of distances from center to points on the perimeter
-# texture_sestandard error for standard deviation of gray-scale values
-# perimeter_se
-# area_se
-# smoothness_sestandard error for local variation in radius lengths
-# compactness_sestandard error for perimeter^2 / area - 1.0
-# concavity_sestandard error for severity of concave portions of the contour
-# concave points_sestandard error for number of concave portions of the contour
-# symmetry_se
-# fractal_dimension_sestandard error for "coastline approximation" - 1
-# radius_worst"worst" or largest mean value for mean of distances from center to points on the perimeter
-# texture_worst"worst" or largest mean value for standard deviation of gray-scale values
-# perimeter_worst
-# area_worst
-# smoothness_worst"worst" or largest mean value for local variation in radius lengths
-# compactness_worst"worst" or largest mean value for perimeter^2 / area - 1.0
-# concavity_worst"worst" or largest mean value for severity of concave portions of the contour
-# concave points_worst"worst" or largest mean value for number of concave portions of the contour
-# symmetry_worst
-# fractal_dimension_worst"worst" or largest mean value for "coastline approximation" - 1
+# mean_radius
+# mean_texture
+# mean_perimeter
+# mean_area
+# mean_smoothness
+# diagnosis
