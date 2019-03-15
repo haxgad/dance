df4fb479a290b74f75fd52268bae4825c0208ae3
Xie Jihui <xjh125470939@gmail.com>
2019-03-15 11:23:49 +0800

    add testing Classifier class, will be remove when integrating

--- software/classifiers/classifier.py
+++ software/classifiers/classifier.py
@@ -9,3 +9,16 @@ class Classifier:
     def predict(self, input_data):
         return self.model.predict_classes(input_data, verbose=0)
 
+
+# test purpose, remove when integrating
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+file_name = 'hard'
+file_path = '../test_data/' + file_name + '.csv'
+dataframe = pd.read_csv(file_path, header=None)
+dataset = dataframe.values
+X = dataset[:,0:len(dataset[0])-1].astype(float)
+y = dataset[:,len(dataset[0])-1].astype(float)
+clf = Classifier('../models/' + file_name + '_model.h5')
+print(confusion_matrix(y, clf.predict(X)))
