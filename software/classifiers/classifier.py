804e3eab76294b4e934e0e0c489e16fab49e2762
Xie Jihui <xjh125470939@gmail.com>
2019-03-19 14:05:19 +0800

    Modify data processing. Features include mean, variance, median, mean absolute deviation, max, min

--- software/classifiers/classifier.py
+++ software/classifiers/classifier.py
@@ -7,21 +7,30 @@ class Classifier:
         print('Successfully loaded the model: ', self.model)
 
     def data_process(input_data):
+        # extract features from raw data
+        # mean, variance, median, mean absolute deviation, max, min
         X = []
         for i in range(len(input_data[0])):
             data = []
             for j in range(len(input_data)):
                 data.append(input_data[j][i])
+            # mean
             X.append(np.mean(data))
+            # variance
             X.append(np.var(data))
+            # median
             X.append(np.median(data))
+            # mean absolute deviation
+            X.append(np.mean(np.absolute(data - np.mean(data))))
+            # max
             X.append(max(data))
+            # min
             X.append(min(data))
         return np.array(X)
 
     def predict(self, input_data):
-        featrues = data_process(input_data)
-        return self.model.predict_classes(featrues, verbose=0)
+        # featrues = data_process(input_data)
+        return self.model.predict_classes(input_data, verbose=0)
 
 
 # test purpose, remove when integrating
@@ -48,8 +57,6 @@ file_path = '../test_data/' + file_name + '.csv'
 dataframe = pd.read_csv(file_path, header=None)
 dataset = dataframe.values
 X = dataset[:,0:len(dataset[0])-1].astype(float)
-# y = dataset[:,len(dataset[0])-1].astype(float)
-# clf = Classifier('../models/' + file_name + '_model.h5')
-# print(confusion_matrix(y, clf.predict(X)))
-X = data_process(X[0:5])
-print(X)
+y = dataset[:,len(dataset[0])-1].astype(float)
+clf = Classifier('../models/' + file_name + '_model.h5')
+print(confusion_matrix(y, clf.predict(X)))
