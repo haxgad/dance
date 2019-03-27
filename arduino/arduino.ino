af81d8d8f8cb04fa9091a42b115134010d4dd5f1
yingchen <lai.yingchen@u.nus.edu>
2019-03-27 19:45:48 +0800

    removed unnecessary code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,8 +2,6 @@
 // Static Variables
 int baudRate = 9600;
 
-//SoftwareSerial mySerial(10, 11); // RX, TX
-
 #include "I2Cdev.h"
 
 #include "MPU6050_6Axis_MotionApps20.h"
@@ -13,8 +11,6 @@ int baudRate = 9600;
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-
-#include "TimerOne.h"
 
 MPU6050 mpu;
 
@@ -42,8 +38,6 @@ VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 const int offset0[6] = {-3273, -1300, 1387, -17, -41, 81};
 const int offset1[6] = {-2734, 2518, 1139, 41, -35, 52};
@@ -51,20 +45,13 @@ const int offset2[6] = {1960, -204, 1074, 203, -144, 25};
 const int offset3[6] = {-3742, 746, 1984, 80, -25, 83};
 const int offset4[6] = {983, -2752, 1027, 273, -40, -159};
 
-const int iAx = 0;
-const int iAy = 1;
-const int iAz = 2;
-const int iGx = 3;
-const int iGy = 4;
-const int iGz = 5;
-
 void SetOffsets(int TheOffsets[6])
-  { mpu.setXAccelOffset(TheOffsets [iAx]);
-    mpu.setYAccelOffset(TheOffsets [iAy]);
-    mpu.setZAccelOffset(TheOffsets [iAz]);
-    mpu.setXGyroOffset (TheOffsets [iGx]);
-    mpu.setYGyroOffset (TheOffsets [iGy]);
-    mpu.setZGyroOffset (TheOffsets [iGz]);
+  { mpu.setXAccelOffset(TheOffsets [0]);
+    mpu.setYAccelOffset(TheOffsets [1]);
+    mpu.setZAccelOffset(TheOffsets [2]);
+    mpu.setXGyroOffset (TheOffsets [3]);
+    mpu.setYGyroOffset (TheOffsets [4]);
+    mpu.setZGyroOffset (TheOffsets [5]);
   } // SetOffsets
 
 
@@ -73,15 +60,6 @@ void SetOffsets(int TheOffsets[6])
 // ================================================================
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-/*
-void dmpDataReady() {    
-    mpuInterrupt = true;
-}*/
-
-
-
-
-char foo;
 
 #include "Arduino.h"
 #include <SoftwareSerial.h>
@@ -92,67 +70,6 @@ char foo;
 // It will be used to ensure only only one Task is accessing this resource at any time.
 SemaphoreHandle_t xSerialSemaphore;
 
-void TaskDigitalRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
-{
-  /*
-    DigitalReadSerial
-    Reads a digital input on pin 2, prints the result to the serial monitor
-
-    This example code is in the public domain.
-  */
-
-  // digital pin 2 has a pushbutton attached to it. Give it a name:
-  uint8_t pushButton = 2;
-
-  // make the pushbutton's pin an input:
-  pinMode(pushButton, INPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    // read the input pin:
-    int buttonState = digitalRead(pushButton);
-
-    // See if we can obtain or "Take" the Serial Semaphore.
-    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-    {
-      // We were able to obtain or "Take" the semaphore and can now access the shared resource.
-      // We want to have the Serial Port for us alone, as it takes some time to print,
-      // so we don't want it getting stolen during the middle of a conversion.
-      // print out the state of the button:
-      Serial.println(buttonState);
-
-      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
-    }
-
-    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
-  }
-}
-
-void TaskAnalogRead( void *pvParameters __attribute__((unused)) )  // This is a Task.
-{
-
-  for (;;)
-  {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-
-    // See if we can obtain or "Take" the Serial Semaphore.
-    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
-    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) 5 ) == pdTRUE )
-    {
-      // We were able to obtain or "Take" the semaphore and can now access the shared resource.
-      // We want to have the Serial Port for us alone, as it takes some time to print,
-      // so we don't want it getting stolen during the middle of a conversion.
-      // print out the value you read:
-      Serial.println(sensorValue);
-
-      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
-    }
-
-    vTaskDelay(200);  // one tick delay (15ms) in between reads for stability
-  }
-}
 // void HandShake( void *pvParameters __attribute__((unused)) ) //http://www.robopapa.com/Projects/RaspberryPiArduinoCommunication
 // {
 //   for (;;)
@@ -165,9 +82,6 @@ void TaskAnalogRead( void *pvParameters __attribute__((unused)) )  // This is a 
 //   vTaskDelay(100);
 //   }  
 // }
-
-int number = 1;
-int i;
 
 // Definition of packet structure
 typedef struct DataPacket{
@@ -370,48 +284,43 @@ void Task1( void *pvParameters __attribute__((unused)) )  // This is a Task.
     
       // otherwise, check for DMP data ready interrupt (this should happen frequently)
       } //else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-      if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-          // wait for correct available data length, should be a VERY short wait
-          while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-    
-          // read a packet from FIFO
-          mpu.getFIFOBytes(fifoBuffer, packetSize);
-          
-          // track FIFO count here in case there is > 1 packet available
-          // (this lets us immediately read more without waiting for an interrupt)
-          fifoCount -= packetSize;
-    
-          // display initial world-frame acceleration, adjusted to remove gravity
-          // and rotated based on known orientation from quaternion
-          mpu.dmpGetQuaternion(&q, fifoBuffer);
-          mpu.dmpGetAccel(&aa, fifoBuffer);
-          mpu.dmpGetGravity(&gravity, &q);
-          mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-          mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-          //Serial.print("aworld\t");
-          double x = (double) aaWorld.x / 8192.0;
-          //Serial.print(x, 5);
-          //Serial.print("\t");
-          double y = (double) aaWorld.y / 8192.0;
-          //Serial.print(y, 5);
-          //Serial.print("\t");
-          double z = (double) aaWorld.z / 8192.0;
-          //Serial.println(z, 5);
-          int arrayCursor = activeSensor * 3;
-          sensorReadings[arrayCursor] = x;
-          sensorReadings[arrayCursor + 1] = y;
-          sensorReadings[arrayCursor + 2] = z;
-          
-      }
-
-      //activeSensor++;
-      //activeSensor %= 5;
-      //Serial.println("Obtained readings");
+        if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+            // wait for correct available data length, should be a VERY short wait
+            while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+      
+            // read a packet from FIFO
+            mpu.getFIFOBytes(fifoBuffer, packetSize);
+            
+            // track FIFO count here in case there is > 1 packet available
+            // (this lets us immediately read more without waiting for an interrupt)
+            fifoCount -= packetSize;
+      
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            //Serial.print("aworld\t");
+            double x = (double) aaWorld.x / 8192.0;
+            //Serial.print(x, 5);
+            //Serial.print("\t");
+            double y = (double) aaWorld.y / 8192.0;
+            //Serial.print(y, 5);
+            //Serial.print("\t");
+            double z = (double) aaWorld.z / 8192.0;
+            //Serial.println(z, 5);
+            int arrayCursor = activeSensor * 3;
+            sensorReadings[arrayCursor] = x;
+            sensorReadings[arrayCursor + 1] = y;
+            sensorReadings[arrayCursor + 2] = z;
+        }
       }
       xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
     }
 
-    vTaskDelay(60);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(4);  // one tick delay (15ms) in between reads for stability
   }
 }
 void Task2( void *pvParameters __attribute__((unused)) )  // This is a Task.
@@ -445,7 +354,7 @@ void Task2( void *pvParameters __attribute__((unused)) )  // This is a Task.
       xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
     }
 
-    vTaskDelay(60);  // one tick delay (15ms) in between reads for stability
+    vTaskDelay(4);  // one tick delay (15ms) in between reads for stability
   }
 }
 
@@ -536,7 +445,7 @@ void setup() {
   }
 
   // Handshake
-  int isReady = 0;
+  int isReady = 1;
 
   while (isReady == 0)
   {
@@ -596,144 +505,4 @@ void setup() {
 // ================================================================
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        if (mpuInterrupt && fifoCount < packetSize) {
-          // try to get out of the infinite loop 
-          fifoCount = mpu.getFIFOCount();
-        }  
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-    }
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-
-    switch (activeSensor) {
-      case 0:
-        digitalWrite(SENSOR0_AD0, LOW);
-        digitalWrite(SENSOR1_AD0, HIGH);
-        digitalWrite(SENSOR2_AD0, HIGH);
-        digitalWrite(SENSOR3_AD0, HIGH);
-        digitalWrite(SENSOR4_AD0, HIGH);
-        SetOffsets(offset0);
-        break;
-      case 1:
-        digitalWrite(SENSOR1_AD0, LOW);
-        digitalWrite(SENSOR0_AD0, HIGH);
-        digitalWrite(SENSOR2_AD0, HIGH);
-        digitalWrite(SENSOR3_AD0, HIGH);
-        digitalWrite(SENSOR4_AD0, HIGH);
-        SetOffsets(offset1);
-        break;
-      case 2:
-        digitalWrite(SENSOR2_AD0, LOW);
-        digitalWrite(SENSOR0_AD0, HIGH);
-        digitalWrite(SENSOR1_AD0, HIGH);
-        digitalWrite(SENSOR3_AD0, HIGH);
-        digitalWrite(SENSOR4_AD0, HIGH);
-        SetOffsets(offset2);
-        break;
-      case 3:
-        digitalWrite(SENSOR3_AD0, LOW);
-        digitalWrite(SENSOR0_AD0, HIGH);
-        digitalWrite(SENSOR1_AD0, HIGH);
-        digitalWrite(SENSOR2_AD0, HIGH);
-        digitalWrite(SENSOR4_AD0, HIGH);
-        SetOffsets(offset3);
-        break;
-      case 4:
-        digitalWrite(SENSOR4_AD0, LOW);
-        digitalWrite(SENSOR0_AD0, HIGH);
-        digitalWrite(SENSOR1_AD0, HIGH);
-        digitalWrite(SENSOR2_AD0, HIGH);
-        digitalWrite(SENSOR3_AD0, HIGH);
-        SetOffsets(offset4);
-        break;
-    }
-
-    //Serial.print("Active Sensor = ");
-    //Serial.println(activeSensor);
-    
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        fifoCount = mpu.getFIFOCount();
-        //Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } //else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-    if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-
-        // display initial world-frame acceleration, adjusted to remove gravity
-        // and rotated based on known orientation from quaternion
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetAccel(&aa, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-        mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-        //Serial.print("aworld\t");
-        double x = (double) aaWorld.x / 8192.0;
-        //Serial.print(x, 5);
-        //Serial.print("\t");
-        double y = (double) aaWorld.y / 8192.0;
-        //Serial.print(y, 5);
-        //Serial.print("\t");
-        double z = (double) aaWorld.z / 8192.0;
-        //Serial.println(z, 5);
-
-        sensorReadings[activeSensor] = x;
-        sensorReadings[activeSensor + 1] = y;
-        sensorReadings[activeSensor + 2] = z;
-        /*
-        double readings[3] = {x, y, z};
-        char deviceCode[1] = {'w'};
-        char buffer[64];
-        unsigned len = sendConfig(buffer,deviceCode,readings);
-        sendSerialData(buffer,len);
-        DataPacket results; 
-        deserialize(&results, buffer);
-        
-        Serial.print("Readings 0 is ");
-        Serial.println(results.readings0[0]);
-        Serial.print(" Readings 1 is ");
-        Serial.println(results.readings0[1]);*/
-    }
-
-    if(activeSensor == 4) {
-        for(int i=0; i<14; i++) {
-            Serial.print(sensorReadings[i], 5);
-            Serial.print("\t");
-        }
-        Serial.println(sensorReadings[14], 5);
-    }
-
-    activeSensor++;
-    activeSensor %= 5;
 }
