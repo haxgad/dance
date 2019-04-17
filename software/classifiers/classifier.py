7bbe691cc9a5d043090e12d661b077a4a76c07e3
Xie Jihui <xjh125470939@gmail.com>
2019-04-17 16:46:17 +0800

    no message

--- software/classifiers/classifier.py
+++ software/classifiers/classifier.py
@@ -34,23 +34,3 @@ class Classifier:
         features = preprocessing.normalize([self.data_process(input_data)])
         return self.model.predict_classes(features, verbose=0)
 
-
-# test purpose, remove when integrating
-import pandas as pd
-# from sklearn.metrics import confusion_matrix
-
-# file_name = 'dance_data_Mar_26'
-# file_path = '../dance_data/' + file_name + '.csv'
-# dataframe = pd.read_csv(file_path, header=None)
-# dataset = dataframe.values
-# X = preprocessing.normalize(dataset[:,0:len(dataset[0])-1].astype(float))
-# y = dataset[:,len(dataset[0])-1].astype(float)
-# clf = Classifier('../models/' + file_name + '_model.h5')
-# print(confusion_matrix(y, clf.predict(X)))
-
-dataframe = pd.read_csv("../dance_data/processed_data/everything.txt", sep=",", header=None)
-dataset = dataframe.values[:,0:15]
-clf = Classifier('../models/dance_data_Mar_26_model.h5')
-start = 157
-window_size = 25
-print(clf.predict(dataset[start:start+window_size]))
