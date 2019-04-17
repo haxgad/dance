53b4cb3f8bdaffb06c65ab43f98c0a5890d6b5d4
Xie Jihui <xjh125470939@gmail.com>
2019-04-17 16:44:46 +0800

    no message

--- software/classifiers/classifier_new.py
+++ software/classifiers/classifier_new.py
@@ -1,11 +1,11 @@
 import numpy as np
-from keras.models import load_model
 from sklearn import preprocessing
+import pickle
 
 class Classifier:
     def __init__(self, model_path):
         print('Loading model from ' +  model_path)
-        self.model = load_model(model_path)
+        self.model = pickle.load(open(model_path, 'rb'))
         print('Successfully loaded the model: ', self.model)
 
     def data_process(self, input_data):
@@ -32,25 +32,5 @@ class Classifier:
 
     def predict(self, input_data):
         features = preprocessing.normalize([self.data_process(input_data)])
-        return self.model.predict_classes(features, verbose=0)
+        return self.model.predict(features)
 
-
-# test purpose, remove when integrating
-import pandas as pd
-# from sklearn.metrics import confusion_matrix
-
-# file_name = 'dance_data_Mar_26'
-# file_path = '../dance_data/' + file_name + '.csv'
-# dataframe = pd.read_csv(file_path, header=None)
-# dataset = dataframe.values
-# X = preprocessing.normalize(dataset[:,0:len(dataset[0])-1].astype(float))
-# y = dataset[:,len(dataset[0])-1].astype(float)
-# clf = Classifier('../models/' + file_name + '_model.h5')
-# print(confusion_matrix(y, clf.predict(X)))
-
-dataframe = pd.read_csv("../dance_data/processed_data/everything.txt", sep=",", header=None)
-dataset = dataframe.values[:,0:15]
-clf = Classifier('../models/dance_data_Mar_26_model.h5')
-start = 157
-window_size = 25
-print(clf.predict(dataset[start:start+window_size]))
